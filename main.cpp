39e47ed8f9e247762b30d6b2e96f1ae945b8fc50
jungsa <106296196+knkevol@users.noreply.github.com>
2025-09-22 14:18:20 +0900

    입출력과정
    Input
    Process
    Render

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
-#include <iostream>
+#include <iostream> // cpp standard
+#include <conio.h> // c standard
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main()
 {
+	//LoadLevel
 	int Map[10][10] = 
 	{
 		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
@@ -21,25 +25,54 @@ int main()
 	int PlayerX = 1;
 	int PlayerY = 1;
 	char PlayerShape = 'P';
+	bool bIsPlaying = true;
 
 	//»ç»ó(Mapping)
 	char Sprites[10] = { ' ', '*', };
 
-	//Render();
-	for (int Y = 0; Y < 10; Y++)
+	//Input
+	while (bIsPlaying)
 	{
-		for (int X = 0; X < 10; X++)
+		int KeyCode = _getch();
+	
+		if (KeyCode == 'w')
 		{
-			if (PlayerX == X && PlayerY == Y)
-			{
-				cout << PlayerShape;
-			}
-			else
-			{
-				cout << Sprites[Map[Y][X]];
-			}
+			PlayerY--;
 		}
-		cout << "\n";
+		else if (KeyCode == 's')
+		{
+			PlayerY++;
+		}
+		else if (KeyCode == 'a')
+		{
+			PlayerX--;
+		}
+		else if (KeyCode == 'd')
+		{
+			PlayerX++;
+		}
+		else if (KeyCode == 'q')
+		{
+			bIsPlaying = false;
+		}
+
+		//Render();
+		system("cls");
+		for (int Y = 0; Y < 10; Y++)
+		{
+			for (int X = 0; X < 10; X++)
+			{
+				if (PlayerX == X && PlayerY == Y)
+				{
+					cout << PlayerShape;
+				}
+				else
+				{
+					cout << Sprites[Map[Y][X]];
+				}
+			}
+			cout << "\n";
+		}
 	}
 
 	return 0;
