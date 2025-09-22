ccb269c6e116f1349308a21778990ba304bd8a6c
jungsa <106296196+knkevol@users.noreply.github.com>
2025-09-22 11:03:11 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,16 +4,23 @@ using namespace std;
 
 int main()
 {
+	int NumArray[10] = { 0, };
 
-	int total = 0;
-
-	for (int i = 0; i <= 100; i+=3)
+	for (int j = 0; j < 5; j++)
 	{
-		total += i;
+		for (int i = 0; i < j; ++i)
+		{
+			cout << " ";
+			
+		}
+		for (int k = 0; k < 5-j; ++k)
+		{
+			cout << "*";
+		}
 		
+		cout << "\n";
 	}
-
-	cout << total;
+	
 
 	return 0;
 }
